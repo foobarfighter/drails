a9c0215af6ee8845ca42f12c244124c84b577671
Robert Remeika <bob.remeika@gmail.com>
2009-02-08 02:19:38 -0800

    Added synch Request support

--- javascripts/drails/common.js
+++ javascripts/drails/common.js
@@ -2,57 +2,74 @@ dojo.provide("drails.common");
 dojo.require("dojo._base.xhr");
 
 drails._xhrMap = {
-	"before": 			null,
-	"after": 				null,
-	"condition": 		null,
-	"asynchronous": null,
-	"method": 			null,
-	"insertion": 		null,
-	"position": 		null,
-	"form": 				null,
-	"with": 				null,
-	"update": 			null,
-	"script": 			null,
-	"type": 				null
+  "before":       null,
+  "after":        null,
+  "condition":    null,
+  "asynchronous": [ "sync", function(v) { return !v; } ],
+  "method":       null,
+  "insertion":    null,
+  "position":     null,
+  "form":         null,
+  "with":         null,
+  "update":       null,
+  "script":       null,
+  "type":         null
 };
 
 drails._xhrCallbackMap = {
-	"onUninitialized": 	null,
-	"onLoading": 				null,
-	"onLoaded": 				null,
-	"onInteractive": 		null,
-	"onComplete": 			null,
-	"onFailure": 				"error",
-	"onSuccess": 				"load"
-	// TODO 100..599
+  "onUninitialized":  null,
+  "onLoading":        null,
+  "onLoaded":         null,
+  "onInteractive":    null,
+  "onComplete":       null,
+  "onFailure":        "error",
+  "onSuccess":        "load"
+  // TODO 100..599
 };
 
 dojo.declare("drails._base", null, {
-	interpolateXhr: function(url, xhrArgs) {
-		var dojoXhrArgs = { url: url };
-		for (var protoCallback in xhrArgs) {
-			var dojoCallback = drails._xhrCallbackMap[protoCallback];
-			if (dojoCallback) {		// Found callback mapping
-				// If a prototype callback handler exists on this object
-				if (this[protoCallback]) {
-				  dojo.connect(dojoXhrArgs, dojoCallback, this, protoCallback);
-				  if (xhrArgs[protoCallback]){
-					  dojo.connect(this, protoCallback, xhrArgs[protoCallback]);	// Connect the callback to the currently existing callback
-				  }
-				}	else if (xhrArgs[protoCallback]) {
-				  dojoXhrArgs[dojoCallback] = xhrArgs[protoCallback];
-				}
-			}	else {							// Did not find a callback mapping
-				this.unsupportedOperation(protoCallback);
-			}
-		}
-		return dojoXhrArgs;
-	},
-	
-	unsupportedOperation: function(callbackName){
-		throw(callbackName + " is not a supported drails operation");
-	}
-})
+  transformCallbacks: function(url, xhrArgs) {
+    var dojoXhrArgs = { url: url };
+    for (var protoCallback in xhrArgs) {
+      var dojoCallback = drails._xhrCallbackMap[protoCallback];
+      if (dojoCallback) {   // Found callback mapping
+        // If a prototype callback handler exists on this object
+        if (this[protoCallback]) {
+          dojo.connect(dojoXhrArgs, dojoCallback, this, protoCallback);
+          if (xhrArgs[protoCallback]){
+            dojo.connect(this, protoCallback, xhrArgs[protoCallback]);  // Connect the callback to the currently existing callback
+          }
+        } else if (xhrArgs[protoCallback]) {
+          dojoXhrArgs[dojoCallback] = xhrArgs[protoCallback];
+        }
+      } else {              // Did not find a callback mapping
+        this.unsupportedOperation(protoCallback);
+      }
+    }
+    return dojoXhrArgs;
+  },
+  
+  transformSettings: function(xhrArgs){
+    var dojoXhrArgs = {};
+    for (var setting in xhrArgs){
+      var dojoSetting = drails._xhrMap[setting];
+      if (dojoSetting){
+        var value = dojoSetting;
+        if (dojo.isArray(value)) {
+          dojoSetting = value[0];
+          value = value[1](xhrArgs[setting]);
+        }
+        dojoXhrArgs[dojoSetting] = value;
+        delete xhrArgs[setting];
+      }
+    }
+    return dojoXhrArgs;
+  },
+  
+  unsupportedOperation: function(callbackName){
+    throw(callbackName + " is not a supported drails operation");
+  }
+});
 
 dojo.declare("drails.Request", [drails._base], {
   _requestOnConstruction: true,
@@ -64,10 +81,11 @@ dojo.declare("drails.Request", [drails._base], {
   },
   
   xhr: function(url, xhrArgs) {
-    var dojoXhrArgs;
+    var dojoXhrArgs = {};
     
-    if (xhrArgs) dojoXhrArgs = this.interpolateXhr(url, xhrArgs);
-		dojo.xhrGet(dojoXhrArgs);
+    if (xhrArgs) dojo.mixin(dojoXhrArgs, this.transformSettings(xhrArgs));
+    if (xhrArgs) dojo.mixin(dojoXhrArgs, this.transformCallbacks(url, xhrArgs));
+    dojo.xhrGet(dojoXhrArgs);
   }
 });
 
@@ -77,16 +95,16 @@ dojo.declare("drails.Updater", [drails.Request], {
   _requestOnConstruction: false,
   _successNode: null,
   _failureNode: null,
-	
-	constructor: function(target, url, xhrArgs) {
-	  xhrArgs = xhrArgs || {};
-  	xhrArgs['onSuccess'] = xhrArgs['onSuccess'] || function() {};
-  	xhrArgs['onFailure'] = xhrArgs['onFailure'] || function() {};
+  
+  constructor: function(target, url, xhrArgs) {
+    xhrArgs = xhrArgs || {};
+    xhrArgs['onSuccess'] = xhrArgs['onSuccess'] || function() {};
+    xhrArgs['onFailure'] = xhrArgs['onFailure'] || function() {};
 
-  	if (target) this.interpolateTargets(target);
-  	this.xhr(url, xhrArgs);
-	},
-	
+    if (target) this.interpolateTargets(target);
+    this.xhr(url, xhrArgs);
+  },
+  
   onSuccess: function(response, ioArgs) {
     if (this._successNode){
       dojo.byId(this._successNode).innerHTML = response.toString();
@@ -104,7 +122,7 @@ dojo.declare("drails.Updater", [drails.Request], {
       this._successNode = this._failureNode = target;
     } else if (typeof target == "object") {
       this._successNode = target["success"];
-    	this._failureNode = target["failure"];
+      this._failureNode = target["failure"];
     }
     else {
       throw new Error("Invalid target type");
