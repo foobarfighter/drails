f7a4ec6eb147decc8401cc91f5001b2b7ddbe727
Robert Remeika <bob.remeika@gmail.com>
2009-02-11 21:39:22 -0800

    Finished specs for PeriodicalExecuter

--- javascripts/drails/tests/module.js
+++ javascripts/drails/tests/module.js
@@ -2,9 +2,9 @@ dojo.provide("drails.tests.module");
 
 try{
   doh.registerUrl("drails.tests.PeriodicalExecuter", dojo.moduleUrl("drails", "tests/PeriodicalExecuter.html"));
-  //doh.registerUrl("drails.tests.Observer", dojo.moduleUrl("drails", "tests/Observer.html"));
-  //doh.registerUrl("drails.tests.Updater", dojo.moduleUrl("drails", "tests/Updater.html"));
-  //doh.registerUrl("drails.tests.Request", dojo.moduleUrl("drails", "tests/Request.html"));
+  doh.registerUrl("drails.tests.Observer", dojo.moduleUrl("drails", "tests/Observer.html"));
+  doh.registerUrl("drails.tests.Updater", dojo.moduleUrl("drails", "tests/Updater.html"));
+  doh.registerUrl("drails.tests.Request", dojo.moduleUrl("drails", "tests/Request.html"));
 }catch(e){
   doh.debug(e);
 }
