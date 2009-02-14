173836b83c4f145117573e0e77e138b38d012641
Robert Remeika <bob.remeika@gmail.com>
2009-02-13 21:56:42 -0800

    Revert "Revert "Added spec skeleton for for drails.Form.Element.EventObserver""
    This reverts commit a9a8b2c9a44c2b528db3d6dc5170f98a088113ce.

--- javascripts/drails/tests/module.js
+++ javascripts/drails/tests/module.js
@@ -1,14 +1,15 @@
 dojo.provide("drails.tests.module");
 
 try{
-  //doh.registerUrl("drails.tests.PeriodicalExecuter", dojo.moduleUrl("drails", "tests/PeriodicalExecuter.html"));
+  doh.registerUrl("drails.tests.PeriodicalExecuter", dojo.moduleUrl("drails", "tests/PeriodicalExecuter.html"));
   doh.registerUrl("drails.tests.EventObserver", dojo.moduleUrl("drails", "tests/EventObserver.html"));
-  // doh.registerUrl("drails.tests.Form.Observer", dojo.moduleUrl("drails", "tests/Form.Observer.html"));
-  //   doh.registerUrl("drails.tests.Form.Element.Observer", dojo.moduleUrl("drails", "tests/Form.Element.Observer.html"));
-  //   doh.registerUrl("drails.tests.TimedObserver", dojo.moduleUrl("drails", "tests/TimedObserver.html"));
-  //   doh.registerUrl("drails.tests.Observer", dojo.moduleUrl("drails", "tests/Observer.html"));
-  //   doh.registerUrl("drails.tests.Updater", dojo.moduleUrl("drails", "tests/Updater.html"));
-  //   doh.registerUrl("drails.tests.Request", dojo.moduleUrl("drails", "tests/Request.html"));
+  doh.registerUrl("drails.tests.Form.Element.EventObserver", dojo.moduleUrl("drails", "tests/Form.Element.EventObserver.html"));
+  doh.registerUrl("drails.tests.Form.Observer", dojo.moduleUrl("drails", "tests/Form.Observer.html"));
+  doh.registerUrl("drails.tests.Form.Element.Observer", dojo.moduleUrl("drails", "tests/Form.Element.Observer.html"));
+  doh.registerUrl("drails.tests.TimedObserver", dojo.moduleUrl("drails", "tests/TimedObserver.html"));
+  doh.registerUrl("drails.tests.Observer", dojo.moduleUrl("drails", "tests/Observer.html"));
+  doh.registerUrl("drails.tests.Updater", dojo.moduleUrl("drails", "tests/Updater.html"));
+  doh.registerUrl("drails.tests.Request", dojo.moduleUrl("drails", "tests/Request.html"));
 }catch(e){
   doh.debug(e);
 }
