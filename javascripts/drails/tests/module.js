8e55b805c48686dde9b2672e69c8e6bb56695dc3
Robert Remeika <bob.remeika@gmail.com>
2009-02-23 00:07:48 -0800

    All specs pass

--- javascripts/drails/tests/module.js
+++ javascripts/drails/tests/module.js
@@ -1,14 +1,14 @@
 dojo.provide("drails.tests.module");
 
 try{
-	// doh.registerUrl("drails.tests.PeriodicalExecuter", dojo.moduleUrl("drails", "tests/PeriodicalExecuter.html"));
-	// doh.registerUrl("drails.tests.Updater", dojo.moduleUrl("drails", "tests/Updater.html"));
-	// doh.registerUrl("drails.tests.Request", dojo.moduleUrl("drails", "tests/Request.html"));
-	// doh.registerUrl("drails.tests.Effect", dojo.moduleUrl("drails", "tests/Effect.html"));
-	// doh.registerUrl("drails.tests.dnd.Source", dojo.moduleUrl("drails", "tests/dnd.Source.html"));
-	// doh.registerUrl("drails.tests.Draggable", dojo.moduleUrl("drails", "tests/Draggable.html"));
-	// doh.registerUrl("drails.tests.Droppable", dojo.moduleUrl("drails", "tests/Droppable.html"));
-	// doh.registerUrl("drails.tests.Sortable", dojo.moduleUrl("drails", "tests/Sortable.html"));
+	doh.registerUrl("drails.tests.PeriodicalExecuter", dojo.moduleUrl("drails", "tests/PeriodicalExecuter.html"));
+	doh.registerUrl("drails.tests.Updater", dojo.moduleUrl("drails", "tests/Updater.html"));
+	doh.registerUrl("drails.tests.Request", dojo.moduleUrl("drails", "tests/Request.html"));
+	doh.registerUrl("drails.tests.Effect", dojo.moduleUrl("drails", "tests/Effect.html"));
+	doh.registerUrl("drails.tests.dnd.Source", dojo.moduleUrl("drails", "tests/dnd.Source.html"));
+	doh.registerUrl("drails.tests.Draggable", dojo.moduleUrl("drails", "tests/Draggable.html"));
+	doh.registerUrl("drails.tests.Droppable", dojo.moduleUrl("drails", "tests/Droppable.html"));
+	doh.registerUrl("drails.tests.Sortable", dojo.moduleUrl("drails", "tests/Sortable.html"));
 	doh.registerUrl("drails.tests.EventObserver", dojo.moduleUrl("drails", "tests/EventObserver.html"));
 	doh.registerUrl("drails.tests.Form.Element.EventObserver", dojo.moduleUrl("drails", "tests/Form.Element.EventObserver.html"));
 	doh.registerUrl("drails.tests.Form.EventObserver", dojo.moduleUrl("drails", "tests/Form.EventObserver.html"));
