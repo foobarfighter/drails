a742099fed8f105caa5c8024313a5aa755c34b42
Robert Remeika <bob.remeika@gmail.com>
2009-03-27 14:00:14 -0700

    Changed this to a tiny release

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -1,6 +1,6 @@
 describe Drails do
   it "reports the correct version information" do
-    expected_version_string = "1.1.0"
+    expected_version_string = "1.0.1"
     Drails::VERSION::STRING.should == expected_version_string
     expected_version_array = expected_version_string.split(".").collect { |part| part.to_i }
     Drails::VERSION::MAJOR.should == expected_version_array[0]
